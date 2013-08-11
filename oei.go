344f8d6f6f436701fad39f0d60d3d9863f1d1236
Chris Oei <code@oei.io>
2013-08-11 14:43:20 -0700

    Fix comments

--- oei.go
+++ oei.go
@@ -5,12 +5,14 @@ import (
 	"os/exec"
 )
 
+// Simple error handler that prints the error and exits the program
 func ErrorHandler(err error) {
 	if err != nil {
 		log.Fatalln("Error: ", err)
 	}
 }
 
+// Run a command with arguments, print the output, exit if any errors, return result
 func AssertExec(command string, args ...string) string {
 	output, err := exec.Command(command, args...).Output()
 	outputString := string(output)
